5072cc2a7ddd7516518fa68655c38b9cf7ab94d9
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-09-18 10:56:24 +0300

    Improve the commentary.

--- llama-cpp.el
+++ llama-cpp.el
@@ -23,7 +23,8 @@
 
 ;;; Commentary:
 
-;; llama-cpp client
+;; An Emacs client for interacting with the `llama-cpp' server:
+;; https://github.com/ggerganov/llama.cpp/tree/master/examples/server
 
 ;;; Code:
 
