20b588549ab35fe5c5dba4171443c2508d8dc14a
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-10-07 03:35:33 +0300

    Don't enable mirostat by default.
    It requires to set target entropy which might differ for different models.

--- llama-cpp.el
+++ llama-cpp.el
@@ -45,7 +45,7 @@
   :type 'natnum
   :group 'llama)
 
-(defcustom llama-cpp-params '(:n_predict -1 :mirostat 2)
+(defcustom llama-cpp-params '(:n_predict -1)
   "Parameters for the llama-cpp /completion request."
   :type '(alist :key-type (symbol :tag "Parameter")
                 :value-type (sexp :tag "Value"))
